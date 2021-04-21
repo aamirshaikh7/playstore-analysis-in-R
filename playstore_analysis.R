fdfc848301a14b734f3b0c6bee9713201ada6bb6
aamirshaikh7 <aamirshaikh3232@gmail.com>
2021-04-21 22:49:41 +0530

    handling missing values

--- playstore_analysis.R
+++ playstore_analysis.R
@@ -47,3 +47,21 @@ View(missingdata)
 df2[df2$Rating > 5]
 
 sum(df2$Rating > 5)
+
+# 1. clear all missing values
+
+clean_data <- na.omit(df)
+
+sum(is.na(clean_data))
+
+View(clean_data)
+
+# 2. Replacing missing value with mean or median
+
+df2 <- df
+
+for (i in which(sapply(df2, is.numeric))) {
+  df2[is.na(df2[, i]), i] <- median(df2[, i], na.rm = TRUE)
+}
+
+sum(is.na(df2))
